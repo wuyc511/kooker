4ef26f096a820817d125dcd0443f7d7fe598edb8
wuyongchang <wuyc511@foxmail.com>
2020-07-24 10:03:40 +0800

    start

---
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@MapperScan("com.example.demo.mapper")
+@ComponentScan("com.example.demo.*")
+public class Demo1Application {
+
+	public static void main(String[] args) {
+		System.out.println("---------------------") ;
+		SpringApplication.run(Demo1Application.class, args);
+	}
+
+}
